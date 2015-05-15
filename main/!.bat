4c4b12c6e9a0101f902d62fd0d5045160168eeea
kapitalsoft <kapitalsoft@gmail.com>
2015-05-15 12:02:23 +0200

    Delete !.bat

--- main/!.bat
+++
@@ -1,7 +0,0 @@
-@d:\php\php.exe index.php > index.html
-@d:\php\php.exe proizvodi.php > proizvodi.html
-@d:\php\php.exe reference.php > reference.html
-@d:\php\php.exe hardware.php > hardware.html
-@d:\php\php.exe portfolio.php > portfolio.html
-@d:\php\php.exe kontakt.php > kontakt.html
-
