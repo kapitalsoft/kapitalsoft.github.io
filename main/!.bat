b588d9de0ce2c30256ca998c5ed6fbe6bcce80cc
kapitalsoft <kapitalsoft@gmail.com>
2015-05-15 11:52:30 +0200

    ...

---
+++ main/!.bat
@@ -0,0 +1,7 @@
+@d:\php\php.exe index.php > index.html
+@d:\php\php.exe proizvodi.php > proizvodi.html
+@d:\php\php.exe reference.php > reference.html
+@d:\php\php.exe hardware.php > hardware.html
+@d:\php\php.exe portfolio.php > portfolio.html
+@d:\php\php.exe kontakt.php > kontakt.html
+
